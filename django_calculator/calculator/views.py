5957703e548a49f472aecc73a2ffbbb0a770ff05
BarnetteME1 <BarnetteME1@gmail.com>
2015-10-20 15:30:31 -0400

    Put in error message if input is not a number

---
+++ django_calculator/calculator/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render_to_response
+
+__author__ = 'MatthewBarnette'
+
+def math(request):
+    first = request.GET.get('first_number')
+    second = request.GET.get('second_number')
+    operator = request.GET.get('operator')
+    op_dict = {'plus': "+", 'minus': "-", 'multiply': "*", 'divided': '/'}
+    if operator == None:
+        context = {'answer': ''}
+        return render_to_response(template_name='calculator.html', context=context)
+    else:
+        equation = first + op_dict[operator] + second
+        try:
+            answer = float(eval(equation))
+        except:
+            context = {'answer': 'Cannot be calculated!!'}
+            return render_to_response(template_name='calculator.html', context=context)
+    context = {'answer': answer'!f'}
+    return render_to_response(template_name='calculator.html', context=context)
