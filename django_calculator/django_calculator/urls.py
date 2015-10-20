6d9f41a3e6db9453785dd044754f684f0f44fec0
BarnetteME1 <BarnetteME1@gmail.com>
2015-10-20 12:56:07 -0400

    Page running

--- django_calculator/django_calculator/urls.py
+++ django_calculator/django_calculator/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from calculator.views import index_view
 
 urlpatterns = [
-    url(r'^$', index_view)
+    url(r'^$', index_view),
     url(r'^admin/', include(admin.site.urls)),
 ]
